13d958f7acbcf53ea5ce5c811d3fc4b92016ac44
Awesome <awesome_devnet@outlook.com>
2024-02-13 15:10:08 +0900

    fix: update ImageMaker

--- tools/ImageMaker/imageMaker.c
+++ tools/ImageMaker/imageMaker.c
@@ -9,133 +9,112 @@
 
 #define BYTESOFSECTOR 512
 
-int AdjustInSectorSize(int iFd, int iSourceSize);
-void WriteKernelInformation(int iTargetFd, int iKernelSectorCount);
-int CopyFile(int iSourceFd, int iTargetFd);
+int adjustInSectorSize(int fd, int sourceSize);
+void writeKernelInfo(int targetFd, int kernelSectorCount);
+int copyFile(int sourceFd, int targetFd);
 
 // main 함수
 int main(int argc, char* argv[]) {
-    int iSourceFd;
-    int iTargetFd;
+    int sourceFd;
+    int targetFd;
     int iBootLoaderSize;
     int iKernel32SectorCount;
-    int iSourceSize;
+    int sourceSize;
 
-    // 커맨드 라인 옵션 검사
+    // process argument
     if (argc < 3) {
         fprintf(stderr, "[ERROR] ImageMaker.exe BootLoader.bin Kernel32.bin\n");
         exit(-1);
     }
 
-    // Disk.img 파일을 생성
-    if ((iTargetFd = open("build/OS.img", O_RDWR | O_CREAT | O_TRUNC | S_IREAD | S_IWRITE)) == -1) {
+    // create disk image
+    if ((targetFd = open("build/OS.img", O_RDWR | O_CREAT | O_TRUNC | S_IREAD | S_IWRITE)) == -1) {
         fprintf(stderr, "[ERROR] Disk.img open fail.\n");
         exit(-1);
     }
 
-    //--------------------------------------------------------------------------
-    // 부트 로더 파일을 열어서 모든 내용을 디스크 이미지 파일로 복사
-    //--------------------------------------------------------------------------
+
     printf("[INFO] Copy boot loader to image file\n");
-    if ((iSourceFd = open(argv[1], O_RDONLY)) == -1) {
+    if ((sourceFd = open(argv[1], O_RDONLY)) == -1) {
         fprintf(stderr, "[ERROR] %s open fail\n", argv[1]);
         exit(-1);
     }
 
-    iSourceSize = CopyFile(iSourceFd, iTargetFd);
-    close(iSourceFd);
+    sourceSize = copyFile(sourceFd, targetFd);
+    close(sourceFd);
 
-    // 파일 크기를 섹터 크기인 512바이트로 맞추기 위해 나머지 부분을 0x00 으로
-    // 채움
-    iBootLoaderSize = AdjustInSectorSize(iTargetFd, iSourceSize);
+    iBootLoaderSize = adjustInSectorSize(targetFd, sourceSize);
     printf("[INFO] %s size = [%d] and sector count = [%d]\n", argv[1],
-           iSourceSize, iBootLoaderSize);
+           sourceSize, iBootLoaderSize);
 
-    //--------------------------------------------------------------------------
-    // 32비트 커널 파일을 열어서 모든 내용을 디스크 이미지 파일로 복사
-    //--------------------------------------------------------------------------
+    // copy protected mode kernel to image file
     printf("[INFO] Copy protected mode kernel to image file\n");
-    if ((iSourceFd = open(argv[2], O_RDONLY)) == -1) {
+    if ((sourceFd = open(argv[2], O_RDONLY)) == -1) {
         fprintf(stderr, "[ERROR] %s open fail\n", argv[2]);
         exit(-1);
     }
 
-    iSourceSize = CopyFile(iSourceFd, iTargetFd);
-    close(iSourceFd);
+    sourceSize = copyFile(sourceFd, targetFd);
+    close(sourceFd);
 
-    // 파일 크기를 섹터 크기인 512바이트로 맞추기 위해 나머지 부분을 0x00 으로
-    // 채움
-    iKernel32SectorCount = AdjustInSectorSize(iTargetFd, iSourceSize);
+    // fill the rest of sector with 0x00
+    iKernel32SectorCount = adjustInSectorSize(targetFd, sourceSize);
     printf("[INFO] %s size = [%d] and sector count = [%d]\n", argv[2],
-           iSourceSize, iKernel32SectorCount);
+           sourceSize, iKernel32SectorCount);
 
-    //--------------------------------------------------------------------------
-    // 디스크 이미지에 커널 정보를 갱신
-    //--------------------------------------------------------------------------
+    // refresh the disk image file
     printf("[INFO] Start to write kernel information\n");
     // 부트섹터의 5번째 바이트부터 커널에 대한 정보를 넣음
-    WriteKernelInformation(iTargetFd, iKernel32SectorCount);
+    writeKernelInfo(targetFd, iKernel32SectorCount);
     printf("[INFO] Image file create complete\n");
 
-    close(iTargetFd);
+    close(targetFd);
     return 0;
 }
 
-/**
- *  현재 위치부터 512바이트 배수 위치까지 맞추어 0x00으로 채움
- */
-int AdjustInSectorSize(int iFd, int iSourceSize) {
+int adjustInSectorSize(int fd, int sourceSize) {
     int i;
-    int iAdjustSizeToSector;
-    char cCh;
+    int adjustSizeToSector;
+    char ch;
     int iSectorCount;
 
-    iAdjustSizeToSector = iSourceSize % BYTESOFSECTOR;
-    cCh = 0x00;
+    adjustSizeToSector = sourceSize % BYTESOFSECTOR;
+    ch = 0x00;
 
-    if (iAdjustSizeToSector != 0) {
-        iAdjustSizeToSector = 512 - iAdjustSizeToSector;
-        printf("[INFO] File size [%lu] and fill [%u] byte\n", iSourceSize,
-               iAdjustSizeToSector);
-        for (i = 0; i < iAdjustSizeToSector; i++) {
-            write(iFd, &cCh, 1);
+    if (adjustSizeToSector != 0) {
+        adjustSizeToSector = 512 - adjustSizeToSector;
+        printf("[INFO] File size [%lu] and fill [%u] byte\n", sourceSize,
+               adjustSizeToSector);
+        for (i = 0; i < adjustSizeToSector; i++) {
+            write(fd, &ch, 1);
         }
     } else {
         printf("[INFO] File size is aligned 512 byte\n");
     }
 
-    // 섹터 수를 되돌려줌
-    iSectorCount = (iSourceSize + iAdjustSizeToSector) / BYTESOFSECTOR;
+    iSectorCount = (sourceSize + adjustSizeToSector) / BYTESOFSECTOR;
     return iSectorCount;
 }
 
-/**
- *  부트 로더에 커널에 대한 정보를 삽입
- */
-void WriteKernelInformation(int iTargetFd, int iKernelSectorCount) {
+void writeKernelInfo(int targetFd, int kernelSectorCount) {
     unsigned short usData;
     long lPosition;
 
-    // 파일의 시작에서 5바이트 떨어진 위치가 커널의 총 섹터 수 정보를 나타냄
-    lPosition = lseek(iTargetFd, 5, SEEK_SET);
+    lPosition = lseek(targetFd, 5, SEEK_SET);
     if (lPosition == -1) {
         fprintf(stderr, "lseek fail. Return value = %d, errno = %d, %d\n",
                 lPosition, errno, SEEK_SET);
         exit(-1);
     }
 
-    usData = (unsigned short)iKernelSectorCount;
-    write(iTargetFd, &usData, 2);
+    usData = (unsigned short)kernelSectorCount;
+    write(targetFd, &usData, 2);
 
     printf("[INFO] Total sector count except boot loader [%d]\n",
-           iKernelSectorCount);
+           kernelSectorCount);
 }
 
-/**
- *  소스 파일(Source FD)의 내용을 목표 파일(Target FD)에 복사하고 그 크기를
- * 되돌려줌
- */
-int CopyFile(int iSourceFd, int iTargetFd) {
+int copyFile(int sourceFd, int targetFd) {
     int iSourceFileSize;
     int iRead;
     int iWrite;
@@ -143,8 +122,8 @@ int CopyFile(int iSourceFd, int iTargetFd) {
 
     iSourceFileSize = 0;
     while (1) {
-        iRead = read(iSourceFd, vcBuffer, sizeof(vcBuffer));
-        iWrite = write(iTargetFd, vcBuffer, iRead);
+        iRead = read(sourceFd, vcBuffer, sizeof(vcBuffer));
+        iWrite = write(targetFd, vcBuffer, iRead);
 
         if (iRead != iWrite) {
             fprintf(stderr, "[ERROR] iRead != iWrite.. \n");
